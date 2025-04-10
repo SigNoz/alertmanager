b5d26c1752c0eb4e38419ba558b1a671f2abba58
Vikrant Gupta <vikrant@signoz.io>
2025-04-10 12:55:48 +0530

    fix(alertmanager): update the channel entity id (#41)

--- integrations/queryservice/types.go
+++ integrations/queryservice/types.go
@@ -5,7 +5,7 @@ type channelResponse struct {
 }
 
 type channelItem struct {
-	Id        int       `json:"id" db:"id"`
+	Id        string       `json:"id" db:"id"`
 	Name      string    `json:"name" db:"name"`
 	Type      string    `json:"type" db:"type"`
 	Data      string    `json:"data" db:"data"`
